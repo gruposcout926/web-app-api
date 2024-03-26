98d1068afe98b1c03aef1e339a158057d5d1be8c
Emanuel Ceballos <ema.ceballos@gmail.com>
2024-03-26 02:38:13 -0300

    Fallback port: 80

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    await app.listen(process.env.APP_PORT || 3001);
+    await app.listen(process.env.APP_PORT || 80);
+
+    console.log(`process.env.APP_PORT: ${process.env.APP_PORT}`);
 
     console.log(`Application running at ${await app.getUrl()}`, 'Starting App');
 }
