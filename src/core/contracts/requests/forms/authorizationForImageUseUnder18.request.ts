b2a01b1cbb179fb7ec32d1850987c237d13eb443
Emanuel Ceballos <ema.ceballos@gmail.com>
2024-04-24 11:43:20 -0300

    GS-15 removed forms module

--- src/core/contracts/requests/forms/authorizationForImageUseUnder18.request.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean } from 'class-validator';
-
-export class AuthorizationForImageUseUnder18Request {
-    @ApiProperty()
-    @IsBoolean()
-    isAccepted: boolean;
-}
